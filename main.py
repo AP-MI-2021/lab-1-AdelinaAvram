644988bd97a945930d475450ec39bbad4d05785c
AdelinaAvram <91989340+AdelinaAvram@users.noreply.github.com>
2021-10-06 01:47:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,50 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  
+if n <= 1:
+return False
+if n == 2:
+return True
+div = 2
+while div * div <= n:
+if n % div == 0:
+return False
+div = div + 1
+ return True
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+prod = 1
+for i in lst:
+prod = prod * i
+return prod
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+r = x % y
+while y != 0:
+r = x % y
+x = y
+y = r
+return x
+
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  while x != y:
+if(x > y):
+x = x - y
+else:
+y = y - x
+return x
   
   
 def main():
@@ -31,3 +53,5 @@ def main():
 
 if __name__ == '__main__':
   main()
+print(get_cmmdc_v1(8, 9))
+print (get_cmmdc_v2(8, 10))
