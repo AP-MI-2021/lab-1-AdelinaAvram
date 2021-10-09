57cf2cb3102bc7ae644ef05c668dfe6854495490
Adelina <antonia.avram@stud.ubbcluj.ro>
2021-10-10 00:10:35 +0300

    lab 1
    ds

--- main.py
+++ main.py
@@ -2,37 +2,37 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-if n <= 1:
-return False
-if n == 2:
-return True
-div = 2
-while div * div <= n:
-if n % div == 0:
-return False
-div = div + 1
-
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    div = 2
+    while div * div <= n:
+        if n % div == 0:
+            return False
+        div = div + 1
+    return True
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-prod = 1
-for i in lst:
-prod = prod * i
-return prod
+    prod = 1
+    for i in lst:
+        prod = prod * i
+    return prod
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-r = x % y
-while y != 0:
-r = x % y
-x = y
-y = r
-return x
+    r = x % y
+    while y != 0:
+        r = x % y
+        x = y
+        y = r
+    return x
 
   
   
@@ -41,17 +41,17 @@ Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   while x != y:
-if(x > y):
-x = x - y
-else:
-y = y - x
-return x
+    if(x > y):
+        x = x - y
+    else:
+        y = y - x
+  return x
   
   
 def main():
   # interfata de tip consola aici
 
-if __name__ == '__main__':
-  main()
-print(get_cmmdc_v1(8, 9))
-print (get_cmmdc_v2(8, 10))
+    if __name__ == '__main__':
+        main()
+        print(get_cmmdc_v1(8, 9))
+        print (get_cmmdc_v2(8, 10))
